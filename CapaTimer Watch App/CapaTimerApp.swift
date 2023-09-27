6422793558a915fa3ac4a8404221a1efaa78a997
Maxim <maslomaxx@gmail.com>
2023-09-27 23:55:30 +0700

    Initial Commit

---
+++ CapaTimer Watch App/CapaTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapaTimerApp.swift
+//  CapaTimer Watch App
+//
+//  Created by max on 27.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CapaTimer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CapaTimer Watch AppUITests/CapaTimer_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CapaTimer_Watch_AppUITestsLaunchTests.swift
+//  CapaTimer Watch AppUITests
+//
+//  Created by max on 27.09.2023.
+//
+
+import XCTest
+
+final class CapaTimer_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
